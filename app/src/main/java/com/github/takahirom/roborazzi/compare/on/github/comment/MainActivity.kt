b40475a4f7eccd098a6f473bc2b17e99d25eb8a2
hiroaki404 <nosho404@gmail.com>
2024-12-04 19:19:39 +0900

    update ui

--- app/src/main/java/com/github/takahirom/roborazzi/compare/on/github/comment/MainActivity.kt
+++ app/src/main/java/com/github/takahirom/roborazzi/compare/on/github/comment/MainActivity.kt
@@ -21,7 +21,7 @@ class MainActivity : ComponentActivity() {
       RoborazziscreenshotcompareongithubcommentsampleTheme {
         // A surface container using the 'background' color from the theme
         Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-          Greeting("Android")
+          Greeting("Android 2")
         }
       }
     }
